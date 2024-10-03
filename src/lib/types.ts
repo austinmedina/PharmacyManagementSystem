0dffb1265553230ac02692e3f5f66792db78a03f
jath03 <jackthughes@yahoo.com>
2024-10-03 11:05:45 -0700

    Partial types file

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,6 +7,28 @@ export type purchaseLogID = number;
 export type itemID = number;
 export type quantity = number;
 
+export enum UserType {
+    Manager,
+    Pharmacist,
+    Cashier,
+    Technician
+};
+
+export enum ProductType {
+    Prescription,
+    NonPrescription    
+};
+
+export enum InventoryAction {
+    In,
+    Out
+};
+
+export enum Log {
+    In,
+    Out
+};
+
 export type Prescription = {
     ID : UserID;
     productID : productID;
@@ -27,7 +49,7 @@ export type LogLogLog = {
     time : Date;
     userID : UserID;
     action : Log;
-}
+};
 
 export type purchaseLogEntry = {
     ID : purchaseLogID;
@@ -36,5 +58,19 @@ export type purchaseLogEntry = {
     totalPrice : number;
     quantity : quantity;
     cartID : string;
+};
+
+export type InventoryLogEntry = {
+    id: inventoryLogID;
+    time: Date;
+    product: productID;
+    action: InventoryAction;
+    quantity: number;
 }
 
+export type Products = {
+    id: productID;
+    name: string;
+    type: ProductType;
+    price: number;
+};
