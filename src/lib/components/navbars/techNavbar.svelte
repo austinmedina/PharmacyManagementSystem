1c8c43c087a9e8f72829b60d08daed8b25eff176
austinmedina <austinmedina@comcast.net>
2024-10-21 13:11:24 -0700

    Ran lint

--- src/lib/components/navbars/techNavbar.svelte
+++ src/lib/components/navbars/techNavbar.svelte
@@ -4,7 +4,8 @@
     <h1>Insert LOGO HERE</h1>
     <div class="flex justify-evenly gap-8">
         <a href="/checkout" class="hover:font-bold">Checkout</a>
-        <a href="/prescriptionPage" class="hover:font-bold">Enter Prescriptions</a>
+        <a href="/prescriptionPage" class="hover:font-bold"
+            >Enter Prescriptions</a>
         <h2>Logs</h2>
     </div>
     <span class="text-sm">LOGOUT</span>
